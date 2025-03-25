ffe0c7b38c374ed14dfe7518cfb38b8fe7f18323
Zaid0412 <108808139+Zaid0412@users.noreply.github.com>
2025-03-25 11:08:40 +0300

    Add validation for comments

--- middlewares/validation.js
+++ middlewares/validation.js
@@ -3,11 +3,6 @@ const { check } = require("express-validator");
 const isReqMsg = "is required";
 const lengthMsg = "should be between 5 and 35 characters";
 
-module.exports.commentValidation = [
-  check("username").exists().withMessage(`Name ${isReqMsg}`),
-  check("comment").exists().withMessage(`Comment ${isReqMsg}`),
-];
-
 module.exports.registerValidation = [
   check("username")
     .exists()
@@ -35,4 +30,9 @@ module.exports.registerValidation = [
       return true;
     })
     .withMessage("Passwords did not match."),
+];
+
+module.exports.commentValidation = [
+  check("username").exists().withMessage(`Name ${isReqMsg}`),
+  check("comment").exists().withMessage(`Comment ${isReqMsg}`),
 ];
